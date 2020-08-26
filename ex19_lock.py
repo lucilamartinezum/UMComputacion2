57c61ea59b3c58d75ab8dc93171576a348c4ae8c
lucilamartinezum <lucilamicaelamartinez@gmail.com>
2020-08-26 10:25:34 -0300

    ex 19 finished

---
+++ ex19_lock.py
@@ -0,0 +1,57 @@
+import getopt
+import sys
+import string
+import random
+import multiprocessing as mp
+import time
+import os
+
+
+def runProcess(r, l, file):
+    letter = random.choice(string.ascii_letters)
+    run_file = open(file, "a")
+    l.acquire()
+    for i in range(int(r)):
+        time.sleep(1)
+        run_file.write(letter)
+        run_file.flush()
+        print("Process:", os.getpid(), "letter:", letter)
+    l.release()
+    run_file.close()
+
+
+def getOpts():
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:], 'n:f:r:', [])
+        return opts
+    except getopt.GetoptError as err:
+        print('Error: ' + str(err))
+        exit()
+
+def main():
+
+    if len(sys.argv[1:]) > 1:
+        proc = 0
+        file = ""
+        iterations = 0
+        opts = getOpts()
+        for (opt, arg) in opts:
+            if opt == "-n":
+                proc = int(arg)
+            if opt == "-f":
+                file = arg
+            if opt == "-r":
+                iterations = int(arg)
+        lock = mp.Lock()
+        for i in range(proc):
+            p = mp.Process(target=runProcess, args=(iterations, lock, file))
+            p.start()
+
+        for i in range(proc):
+            p.join()
+
+    else:
+        print("Try again")
+
+if __name__ == "__main__":
+    main()
